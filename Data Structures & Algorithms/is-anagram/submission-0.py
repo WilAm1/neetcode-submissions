d7e3f2999dd232b0b72ee386a76c32c17e2921af
Dyann <70352471+WilAm1@users.noreply.github.com>
2026-08-23 21:56:08 +0800

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        map = {}
+        for letter in s:
+            if letter in map:
+                map[letter] += 1
+            else:
+                map[letter] = 1
+        for letter in t:
+            if letter not in map:
+                return False
+            map[letter] -=1
+            if map[letter] < 0:
+                return False
+        return True
